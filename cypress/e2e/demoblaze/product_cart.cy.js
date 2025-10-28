3502a0fc2c7f68ae4262344708033e6b9926905c
daniels6738 <danieleao@live.com>
2025-10-28 17:58:16 -0300

    Making one test be more in line with others

--- cypress/e2e/demoblaze/product_cart.cy.js
+++ cypress/e2e/demoblaze/product_cart.cy.js
@@ -1,8 +1,4 @@
 describe('Product Cart', () => {
-  const product = "Samsung galaxy s6";
-  const productPrice = "360";
-  const cartCallToAction = "Add to cart";
-  const cartButtonDashboard = '[id="cartur"]';
 
   beforeEach(() => {
     // Visit the homepage before each test
@@ -11,12 +7,12 @@ describe('Product Cart', () => {
 
   it('should add a product to the cart', () => {
     // Select the product, then add it to the cart
-    cy.get("a").contains(product).click();
-    cy.get("a").contains(cartCallToAction).click();
+    cy.get("a").contains("Samsung galaxy s6").click();
+    cy.get("a").contains("Add to cart").click();
 
     // Go to cart and check product is displayed correctly
-    cy.get(cartButtonDashboard).click();
-    cy.get("td").contains(product).should('be.visible');
-    cy.get("td").contains(productPrice).should('be.visible');
+    cy.get('[id="cartur"]').click();
+    cy.get("td").contains("Samsung galaxy s6").should('be.visible');
+    cy.get("td").contains("360").should('be.visible');
   });
 });
